072b74a332c86f721bca5fc0aeed2262b18c4472
Finlifin <98514319+Finlifin@users.noreply.github.com>
2025-05-17 19:49:20 +0800

    sync recovery

--- Flix/Flix/app/src/main/java/fin/phoenix/flix/data/Account.kt
+++
@@ -1,9 +0,0 @@
-package fin.phoenix.flix.data
-
-import com.google.gson.annotations.SerializedName
-
-
-data class Account(
-    @SerializedName("id") val id: String,
-    @SerializedName("phone_number") val phoneNumber: String
-)
